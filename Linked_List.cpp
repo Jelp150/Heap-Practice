9d3d2ac3335c6456ae0c793689277dfcc958a75b
Logan D <girp477@gmail.com>
2025-11-05 15:01:46 -0600

    Update and rename Linked_List to Linked_List.cpp

---
+++ Linked_List.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Node
+{
+  public:
+  int data;
+  Node * next = nullptr;
+  Node * previous = nullptr;
+  Node(int data)
+  {
+    
+  }
+  
+}
+void add_Node(int number, Node * root)
+{
+Node * temp = root;
+  while (temp->next != nullptr)
+  {
+    temp = temp->next;
+  }
+  Node * 
+
+}
+int main()
+{
+  int q, operation, number;
+  cin >> q >> operation >> number;
+  Node * root = nullptr;
+  switch (operation)
+  {
+    case 1:
+
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+  }
+}
